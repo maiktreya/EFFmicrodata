3d2c7b6cf43c05726135ec2c4da070cc1fa01a0d
maiktreya <miguel.garcia.duch@gmail.com>
2024-07-09 18:15:53 +0200

    last

--- revision.R
+++ revision.R
@@ -6,11 +6,11 @@ library(survey)
 
 years <- c(2002, 2005, 2008, 2011, 2014, 2017, 2020)
 years <- c(2011)
+obj <- list()
 
-for (i in years)
-{
-    sel_year <- i
-    df <- fread(paste0("full/", sel_year, "/otras_secciones_", sel_year, "_imp1.csv"))
-    selection <- colnames(df)[colnames(df) %like% "p2_9"]
-    print(selection)
+for (i in years) {
+    for (j in 1:5) {{ sel_year <- i
+        df <- fread(paste0("full/", sel_year, "/otras_secciones_", sel_year, "_imp", j, ".csv"))
+        selection <- colnames(df)[colnames(df) %like% "p2_9b_"]
+        obj[[j]] <- df[, ..selection] }}
 }
