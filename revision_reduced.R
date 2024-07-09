7e6be0c3973cb51912deef061ce0b31e90962d80
maiktreya <miguel.garcia.duch@gmail.com>
2024-07-09 10:52:42 +0200

    last

--- revision_reduced.R
+++ revision_reduced.R
@@ -4,13 +4,11 @@ library(data.table)
 library(magrittr)
 library(survey)
 
-years <- c(2002, 2005, 2008, 2011, 2014, 2017, 2020)
-years <- c(2011)
+years <- c(2002, 2020)
+sel_year <- years[1]
 
-for (i in years)
-{
-    sel_year <- i
-    df <- fread(paste0("full/", sel_year, "/otras_secciones_", sel_year, "_imp1.csv"))
-    selection <- colnames(df)[colnames(df) %like% "p2_9"]
-    print(selection)
-}
+df <- fread(paste0("datasets/",sel_year ,"-EFF.microdat.csv"))
+
+df_sv <- svydesign(ids = ~1, weights = df$facine3, data = df)
+
+# GINI: working-class wealth of finanacial assets
