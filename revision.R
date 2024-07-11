c4c6fb93f6b8fd8b927b8c386e7bd32d7b16102a
maiktreya <miguel.garcia.duch@gmail.com>
2024-07-11 13:42:08 +0200

    last

--- revision.R
+++ revision.R
@@ -64,6 +64,7 @@ full_mean[[1]]$nsitlabdom %>% unique()
 
 # HY090G1, PL031
 ec_sv <- list()
+df_sv <- list()
 
 for (p in 1:5) {
     transf <- ecv_mean[[p]] %>% data.frame()
@@ -74,4 +75,38 @@ for (p in 1:5) {
     )
 }
 
+
+
+
+
 # P29b % population
+nueva <- list()
+for (i in 1:5){
+for(j in 1:5){
+obj_list[[i]][[j]][is.na(obj_list[[i]][[j]])] <- 0
+obj_list[[i]][[j]][,sum:= as.numeric(p2_9b_1) + as.numeric(p2_9b_2) + as.numeric(p2_9b_3) + as.numeric(p2_9b_4)]
+}
+nueva[[i]] <-  obj_list[[i]][[1]]$sum + obj_list[[i]][[1]]$sum + obj_list[[i]][[1]]$sum + obj_list[[i]][[1]]$sum + obj_list[[i]][[1]]$sum
+part <- data.table(nueva[[i]])
+full_mean[[i]][, p2_9b :=part]
+}
+
+
+for (p in 1:5) {
+    transf <- full_mean[[p]] %>% data.frame()
+    df_sv[[p]] <- svydesign(
+        ids = ~1,
+        data = transf,
+        weights = ~ transf$facine3
+    )
+}
+
+
+
+full_mean[[1]][, homeowner := as.factor(np2_1)]
+nrow(full_mean[[1]][homeowner == 1, ]) / nrow(full_mean[[1]])
+
+
+
+full_mean[[1]] %>% nrow()
+nueva[1] %>% length()
