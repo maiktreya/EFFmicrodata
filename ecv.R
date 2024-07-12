7e193ae1d2480fa6c3f495fbc341a2111e326a1d
maiktreya <miguel.garcia.duch@gmail.com>
2024-07-12 12:43:30 +0200

    last

---
+++ ecv.R
@@ -0,0 +1,59 @@
+## Final Revision 072024 for article "Spain, society of owners" @Miguel-Duch
+library(data.table)
+library(magrittr)
+library(survey)
+
+
+ft_hog <- ft_ren <- ft_ratio <- ecv_mean <- list()
+working_class_size <- sample_size <- working_class_ratio <-  c()
+years <- c( 2005, 2008, 2011, 2014, 2017, 2020) # You can expand this later if
+for (p in seq_along(years)) {
+
+ec <- fread(paste0("EFF-ECVSpain/ECV/output/filtered_data", years[p], ".csv"))
+ecv_mean[[p]] <- data.table(ec)
+
+filtered_data <- ecv_mean[[p]]
+
+filtered_data[filtered_data$PL031 %in% c(2, 5), "PL031"] <- 1
+filtered_data[filtered_data$PL031 %in% c(3, 4), "PL031"] <- 2
+filtered_data[filtered_data$PL040 %in% c(2), "PL031"] <- 3
+filtered_data[filtered_data$PL051 %in% c(1, 11, 12, 13, 14, 15, 16, 17, 18, 19), "PL031"] <- 4
+filtered_data[filtered_data$PL031 %in% c(7), "PL031"] <- 5
+filtered_data[filtered_data$PL031 %in% c(6, 8, 9, 10, 11), "PL031"] <- 6
+filtered_data$PL031 <- factor(filtered_data$PL031,
+    levels = c(1, 2, 3, 4, 5, 6),
+    labels = c("worker", "capitalist", "self-employed", "manager", "retired", "inactive")
+)
+
+filtered_data <- filtered_data %>% data.frame()
+filtered_data_sv <- svydesign(
+    ids = ~1,
+    data = filtered_data,
+    weights = ~ filtered_data$PB040
+)
+
+# Share of income of working class rents income
+ft_ren[[p]] <- cbind(svyby(~HY040N, ~PL031, filtered_data_sv, svymean, keep.names = F, keep.var = F), years[p]) # RENTAL ALQ. V
+ft_hog[[p]] <- cbind(svyby(~HY020, ~PL031, filtered_data_sv, svymean, keep.names = F, keep.var = F), years[p])
+ft_ratio[[p]] <- ft_ren[[p]]$statistic / ft_hog[[p]]$statistic
+}
+
+
+# Working class subsample size
+for (z in seq_along(years)) {
+    working_class_size[z] <- nrow(ecv_mean[[z]][PL031 == 1, 1]) 
+    sample_size[z] <-  nrow(ecv_mean[[z]][, 1])
+    working_class_ratio[z] <- working_class_size[z] / sample_size[z]
+
+}
+
+
+#------------------ECV RESULTS-----------------------------------------------------------#
+
+## ECV sample size
+sample_result_z <- cbind(years, sample_size, working_class_size, working_class_ratio)
+print(sample_result_z)
+
+# rental income for workers
+worker_rental_z  <- cbind(rbindlist(ft_ren), rbindlist(ft_hog))
+print(worker_rental_z)
---
+++ eff.R
@@ -0,0 +1,47 @@
+## Final Revision 072024 for article "Spain, society of owners" @Miguel-Duch
+library(data.table)
+library(magrittr)
+library(survey)
+
+
+obj_list <- list()
+full_mean <- list()
+nueva <- list()
+df_sv <- list()
+working_class_size <- sample_size <- working_class_ratio <- c()
+y_w_owner <- y_owner <- working_class_size <- sample_size <- working_class_ratio <- c()
+years <- c(2002, 2005, 2008, 2011, 2014, 2017, 2020) # You can expand this later if
+
+# Young people homeownership
+for (p in seq_along(years)) {
+    full_mean[[p]] <- fread(paste0("EFFSpain/datasets/", years[p], "-EFF.microdat.csv"))
+    full_mean[[p]][, homeowner := as.factor(np2_1)]
+    full_mean[[p]][, young_homeowner := 0][homeowner == 1 & bage == 1, young_homeowner := 1]
+    full_mean[[p]][, young_w_homeowner := 0][young_homeowner == 1 & nsitlabdom == 1, young_w_homeowner := 1]
+    transf <- full_mean[[p]] %>% data.frame()
+    df_sv[[p]] <- svydesign(
+        ids = ~1,
+        data = transf,
+        weights = ~ transf$facine3
+    )
+    y_owner[p] <- prop.table(svytable(~young_homeowner, subset(df_sv[[p]], bage == 1)))[2]
+    y_w_owner[p] <- prop.table(svytable(~young_w_homeowner, subset(df_sv[[p]], bage == 1)))[2]
+}
+
+
+# Working class subsample size
+for (z in seq_along(years)) {
+    working_class_size[z] <- nrow(full_mean[[z]][nsitlabdom == 1, 1])
+    sample_size[z] <- nrow(full_mean[[z]][, 1])
+    working_class_ratio[z] <- working_class_size[z] / sample_size[z]
+}
+
+#------------------EFF RESULTS-----------------------------------------------------------#
+
+## ECV sample size
+sample_result_z <- cbind(years, sample_size, working_class_size, working_class_ratio)
+print(sample_result_z)
+
+# young homeowners (all and workers1)
+homewoner_results <- cbind(years, y_owner, y_w_owner)
+print(homewoner_results)
