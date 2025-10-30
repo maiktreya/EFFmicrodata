1bc2f179d5aa861175b1c21b6c5916c88dca8f41
il    iciuv <miguel.garcia.duch@gmail.com>
2025-10-30 21:54:36 +0100

    last

--- data-transform/loop-data.R
+++ data-transform/loop-data.R
@@ -1,5 +1,8 @@
 ## THIS PARTIAL SCRIPT SHOULD BE USED TOGETHER WITH EFF_API MAIN ONLY TO INCORPORATE NEW EFF RELEASES OR MODIFY VARIABLE DEFINITIONS. IT USES ORIGINAL BDE .dta FILES (SLOW)
+library(data.table)
+library(magrittr)
 
+sel_year <- "2022"
 path <- paste0("datasets/full/EFF_", sel_year, "/")
 source("src/data_selectors.R")
 
@@ -14,7 +17,5 @@ full_mean <-
         full_selection[, paste0(path, "otras_secciones_imp4.dta")] %>% sapply(as.numeric) +
         full_selection[, paste0(path, "otras_secciones_imp5.dta")] %>% sapply(as.numeric)) / 5
 
-to_export <- data.table(full_mean)$p1_1_1
-dt_eff <- fread(paste0("datasets/", sel_year, "-EFF.microdat.csv"))
-dt_eff$sex <- to_export
-dt_eff %>% fwrite(paste0("datasets/", sel_year, "-EFF.microdat.csv"))
+
+full_mean %>% fwrite(paste0("datasets/", sel_year, "-EFF.microdat.csv"))
