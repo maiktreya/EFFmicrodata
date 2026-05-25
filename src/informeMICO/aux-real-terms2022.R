280f029e580b3e9e164b1168d76d9ad62784d056
maiktreya <garciaduchm@gmail.com>
2026-05-25 17:40:46 +0200

    last

--- src/informeMICO/aux-real-terms2022.R
+++ src/informeMICO/aux-real-terms2022.R
@@ -2,7 +2,7 @@
 library(data.table)
 
 # 1. Load Data
-IPC <- fread("datasets/IPC/base2022.csv")
+ipc <- fread("datasets/IPC/base2022.csv")
 inequality_ten <- fread("out/informeMICO/1-inequality-tenancy.csv")
 inequality_age <- fread("out/informeMICO/2-inequality-age.csv")
 inequality_qui <- fread("out/informeMICO/4-inmo-inequality-quantiles.csv")
@@ -10,17 +10,18 @@ inequality_qui <- fread("out/informeMICO/4-inmo-inequality-quantiles.csv")
 waves <- unique(inequality_age$year)
 
 for (wave in waves) {
-    inequality_age[year == wave, mean_renthog_IPC := mean_renthog * 100 / IPC[Year == wave, IPC]]
-    inequality_age[year == wave, median_renthog_IPC := median_renthog * 100 / IPC[Year == wave, IPC]]
-    inequality_age[year == wave, mean_riquezanet_IPC := mean_riquezanet * 100 / IPC[Year == wave, IPC]] 
-    inequality_age[year == wave, median_riquezanet_IPC := median_riquezanet * 100 / IPC[Year == wave, IPC]]
-    inequality_ten[year == wave, mean_renthog_IPC := mean_renthog * 100 / IPC[Year == wave, IPC]]
-    inequality_ten[year == wave, median_renthog_IPC := median_renthog * 100 / IPC[Year == wave, IPC]]
-    inequality_ten[year == wave, mean_riquezanet_IPC := mean_riquezanet * 100 / IPC[Year == wave, IPC]] 
-    inequality_ten[year == wave, median_riquezanet_IPC := median_riquezanet * 100 / IPC[Year == wave, IPC]]
-    inequality_qui[year == wave, median_riquezainmo_IPC := median_riquezainmo * 100 / IPC[Year == wave, IPC]]
+    inequality_age[year == wave, mean_renthog_IPC := mean_renthog * 100 / ipc[year == wave, ipc]]
+    inequality_age[year == wave, median_renthog_IPC := median_renthog * 100 / ipc[year == wave, ipc]]
+    inequality_age[year == wave, mean_riquezanet_IPC := mean_riquezanet * 100 / ipc[year == wave, ipc]]
+    inequality_age[year == wave, median_riquezanet_IPC := median_riquezanet * 100 / ipc[year == wave, ipc]]
+    inequality_ten[year == wave, mean_renthog_IPC := mean_renthog * 100 / ipc[year == wave, ipc]]
+    inequality_ten[year == wave, median_renthog_IPC := median_renthog * 100 / ipc[year == wave, ipc]]
+    inequality_ten[year == wave, mean_riquezanet_IPC := mean_riquezanet * 100 / ipc[year == wave, ipc]]
+    inequality_ten[year == wave, median_riquezanet_IPC := median_riquezanet * 100 / ipc[year == wave, ipc]]
+    inequality_qui[year == wave, median_riquezainmo_IPC := median_riquezainmo * 100 / ipc[year == wave, ipc]]
 }
 
+# export results in 2022 real euros to csv files
 fwrite(inequality_ten, "out/informeMICO/IPC_2022/1-inequality-tenancy.csv")
 fwrite(inequality_age, "out/informeMICO/IPC_2022/2-inequality-age.csv")
 fwrite(inequality_qui, "out/informeMICO/IPC_2022/4-inmo-inequality-quantiles.csv")
